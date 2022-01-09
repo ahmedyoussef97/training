4a90ef7bf2934324097a6e1de363e466400d52b0
ayoussef0900 <a.youssef@sieraj.com>
2022-01-09 17:19:09 +0200

    add register

--- src/app/@shared/register-form/register-form.component.ts
+++ src/app/@shared/register-form/register-form.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AuthService } from '@app/service/auth.service';
 import { SnackBarComponent } from '../snack-bar/snack-bar.component';
+import { NgForm } from '@angular/forms';
 export class Register {
   FullName: string = "";
-  Phone: string = "";
+  Phone: number = 0;
   Email: string = "";
   GenderId: number = 1;
   Password: string = "";
-  AppLangId: number = 2;
 }
 @Component({
   selector: 'app-register-form',
@@ -17,24 +17,32 @@ export class Register {
 })
 export class RegisterFormComponent implements OnInit {
   register: Register = new Register();
-  message: any;
   durationInSeconds = 5;
-  done: any;
-  hide: boolean = true;
-  confirm_password: number = 0;
+  message: any;
+  hide = true;
+  confirmPassword: string = "";
+  isMatch: boolean = true;
   constructor(public _AuthService: AuthService, private _snackBar: MatSnackBar) { }
-  sure() {
+  sure(form: NgForm) {
+    if (form.invalid) return;
     this._AuthService.register(this.register).subscribe((res: any) => {
       console.log(res);
       if (res.StatusId == 200) {
-        this.message = "تم التسجيل بنجاح"
+        this.message = "تمت الإضافة بنجاح"
       }
       else {
         this.message = "يوجد خطأ ما"
       }
       this.openSnackBar()
     })
+  }
 
+  match() {
+    if (this.register.Password == this.confirmPassword) {
+      this.isMatch = true;
+    } else {
+      this.isMatch = false;
+    }
   }
 
   openSnackBar() {
